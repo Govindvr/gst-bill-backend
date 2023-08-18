f01dcc2a90fb6de4448ced7acc36eb1814c841dc
Govind V R <govindvr02@gmail.com>
2023-08-18 09:26:16 +0530

    fix: cors issue

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const sheetRoutes = require('./src/routes/sheets');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express ();
 
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/sheets', sheetRoutes);
 
 
