0f923a6f84237587f13d88f526b6a4c98a1e0e96
Govind V R <govindvr02@gmail.com>
2024-06-21 00:08:16 +0530

    Database Added

---
+++ stock-maintenance-app/db/database.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Products (
+    product_id SERIAL PRIMARY KEY,
+    code VARCHAR(10) NOT NULL UNIQUE,
+    name VARCHAR(60) NOT NULL,
+    category VARCHAR(30) NOT NULL,
+    price DECIMAL(10, 2),
+    current_stock INT NOT NULL
+);
+
+CREATE TABLE Transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    product_id INT,
+    transaction_type VARCHAR(4) NOT NULL CHECK (transaction_type IN ('ADD', 'SELL', 'LOST')),
+    quantity INT NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    balance_stock INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
