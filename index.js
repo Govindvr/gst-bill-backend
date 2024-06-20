f412b6a0a28c0bf93638d3b65085c2449c3976cc
Govind V R <govindvr02@gmail.com>
2024-06-21 00:09:40 +0530

    Output and Error Return

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sheetRoutes = require('./src/routes/sheets');
+const productRoutes = require('./stock-maintenance-app/routes/products');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -9,7 +10,18 @@ const app = express ();
 app.use(express.json());
 app.use(cors());
 app.use('/api/sheets', sheetRoutes);
+app.use('/api/stock/products', productRoutes);
 
+app.use((err,req,res,next)=>{ 
+  const errorStatus = err.status|| 500;
+  const errorMessage = err.message|| "Something went wrong!";
+  res.status(err.status).json({
+    success:false,
+    status:errorStatus,
+    message:errorMessage,
+    stack:err.stack, 
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
