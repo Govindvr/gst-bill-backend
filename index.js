7fdc85b113d72039f18fd6ad1ef8106cad2c0ad6
Govind V R <govindvr02@gmail.com>
2024-07-16 00:06:12 +0530

    feat: user Auth

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const sheetRoutes = require('./src/routes/sheets');
-const productRoutes = require('./stock-maintenance-app/routes/products');
-const transactionRoutes = require('./stock-maintenance-app/routes/transaction');
+const express = require("express");
+const sheetRoutes = require("./src/routes/sheets");
+const productRoutes = require("./stock-maintenance-app/routes/products");
+const transactionRoutes = require("./stock-maintenance-app/routes/transaction");
+const auth = require("./stock-maintenance-app/routes/auth");
 
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express ();
-
+const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/sheets', sheetRoutes);
-app.use('/api/stock/products', productRoutes);
-app.use('/api/stock/transaction', transactionRoutes);
+app.use("/api/sheets", sheetRoutes);
+app.use("/api/stock/products", productRoutes);
+app.use("/api/stock/transaction", transactionRoutes);
+app.use("/auth", auth);
 
-app.use((err,req,res,next)=>{ 
-  const errorStatus = err.status|| 500;
-  const errorMessage = err.message|| "Something went wrong!";
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
   res.status(err.status).json({
-    success:false,
-    status:errorStatus,
-    message:errorMessage,
-    stack:err.stack, 
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
   });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Listening on PORT : ${PORT}`);
-    console.log(`View devoplment server at http://localhost:${PORT}/`);
-  });
+  console.log(`Listening on PORT : ${PORT}`);
+  console.log(`View devoplment server at http://localhost:${PORT}/`);
+});
